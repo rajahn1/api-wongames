08516400995d393812067ab9ce2718350d6e8086
rajahn01 <rafayuno@gmail.com>
2024-05-11 17:28:07 -0300

    upload files

---
+++ src/admin/app.js
@@ -0,0 +1,61 @@
+import LogoBlack from "./extensions/logo-wongames-black.svg";
+import Icon from "./extensions/icon-wongames.svg";
+import favicon from "./extensions/favicon.png";
+
+const config = {
+  auth: { logo: LogoBlack },
+  menu: { logo: Icon },
+  tutorials: false,
+  head: {
+    favicon: favicon,
+  },
+  // theme: {
+  //   light: {
+  //     colors: {
+  //       primary100: "#F2F2",
+  //       primary200: "#F2F2",
+  //       primary500: "#F2F2",
+  //       primary600: "#F2F2",
+  //       primary700: "#F2F2",
+  //       danger700: "#F2F2",
+  //     },
+  //   },
+  // },
+  locales: [
+    // 'ar',
+    // 'fr',
+    // 'cs',
+    // 'de',
+    // 'dk',
+    // 'es',
+    // 'he',
+    // 'id',
+    // 'it',
+    // 'ja',
+    // 'ko',
+    // 'ms',
+    // 'nl',
+    // 'no',
+    // 'pl',
+    // 'pt-BR',
+    // 'pt',
+    // 'ru',
+    // 'sk',
+    // 'sv',
+    // 'th',
+    // 'tr',
+    // 'uk',
+    // 'vi',
+    // 'zh-Hans',
+    // 'zh',
+  ],
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
